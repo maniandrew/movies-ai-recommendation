2674af63b421d302dac0e77c07408c5de9c78a12
Mani <mani@cortiqahealth.com>
2025-05-29 18:04:40 +0530

    basic setup completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from 'react-router-dom'
-import './App.css'
 import AppRouter from './router'
 
 function App() {
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import '../assets/styles/global.css';
 import styles from './Home.module.css'
-import { BoxArrowInRight, PersonPlus } from 'react-bootstrap-icons';
 
 const Home: React.FC = () => {
-    
     return (
-        <Container className={styles.homeContainer}>
-            <Container className={styles.homeMainContent}>
-                <h1>Face Authentication</h1>
-                <Container className={styles.homeContextText}>
-                    <p>The Facial Recognition-Based Authentication Application is a cutting-edge web application developed using React and face-api.js. The main objective of the application is to offer a reliable and efficient authentication system by analyzing and verifying the user's facial features.</p>
-                </Container>
-                <Container className={styles.homeContextActionButtons}>
-                    <Button className={styles.buttons} >
-                        <PersonPlus size={25} /> Register
-                    </Button>
-                    <Button className={styles.buttons}>
-                        <BoxArrowInRight size={25} /> Login
-                    </Button>
-                </Container>
-            </Container>
-        </Container>
+        <div className={styles.homeContainer}>
+            <h2>welcome to home page</h2>
+        </div>
     )
 }
 
